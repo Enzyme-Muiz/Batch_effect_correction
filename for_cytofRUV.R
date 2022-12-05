b4982c5732d83ae45d2e27b24e1ff1dba2e83cc2
Enzyme-Muiz <muizdeenraji@gmail.com>
2022-12-05 14:49:23 +0000

    added Rstyler

--- for_cytofRUV.R
+++ for_cytofRUV.R
@@ -18,92 +18,94 @@ library(shinycssloaders)
 library(readxl)
 library(stringr)
 source("make_fcs_file.R")
-#source("normalize_function.R")
-loading_data<- function(clusters_nb=20, seed=1234, rep_samples, copy_results_here = "fcs_untransformed"){
-try({
-output_dir="CytofRUV_output"
-if (!dir.exists(output_dir)){
-    dir.create(output_dir)
+# source("normalize_function.R")
+loading_data <- function(clusters_nb = 20, seed = 1234, rep_samples, copy_results_here = "fcs_untransformed") {
+  try({
+    output_dir <- "CytofRUV_output"
+    if (!dir.exists(output_dir)) {
+      dir.create(output_dir)
+    }
+    2 + 2
+    wd_data <- file.path(getwd(), output_dir)
+    print(wd_data)
+    metadata_filename <- "Metadata.xlsx"
+    panel_filename <- "Panel.xlsx"
+    seed <- 1234
+    clusters_nb <- clusters_nb
+    print("abc2")
+    data <- load_data(wd_data, metadata_filename, panel_filename, cofactor = 5)
+    print("abc1")
+    print(data$daf)
+    print(getwd())
+    xx <- read_excel("Panel.xlsx")
+    print(xx$antigen)
+    # xx$fcs_colname
+    print(data$lineage_markers)
+    data$daf <- cluster_data(data$daf, seed,
+      markers_to_use = data$lineage_markers,
+      clusters_nb
+    )
+
+    daf <- data$daf
+    md <- data$md
+    seed <- seed
+    n_subset_marker_specific <- 1000
+    print("abc")
+    TSNE_subset <- 1000
+    print("Running TSNE")
+    daf <- CATALYST::runDR(daf, dr = "TSNE", cells = TSNE_subset)
+
+
+    UMAP_subset <- 1000
+    print("Running UMAP")
+    daf <- runDR(daf, "UMAP", cells = UMAP_subset)
+
+    n_subset <- 1000
+    sub_daf <- daf[, sample(ncol(daf), n_subset)]
+
+    panel <- data$panel
+
+
+
+
+    print("-1")
+    dir_name_norm_data <- "CytofRUV_Norm_data_HC2_all_cl_20"
+    print("0")
+    print(data$daf)
+    clusters <- paste0("meta", as.character(clusters_nb))
+    raw_data <- data.frame(sample = data$daf$sample_id, cluster = cluster_ids(data$daf, clusters), t(SummarizedExperiment::assay(data$daf, "exprs")))
+    print("1")
+    colnames(raw_data) <- gsub("^X", "", colnames(raw_data))
+    print("2")
+    rep_samples <- list(rep_samples)
+    print("3")
+    cluster_list_rep_samples <- list(seq(1, as.numeric(clusters_nb)))
+    print(cluster_list_rep_samples)
+    print("4")
+    k_value <- 5
+    seed <- 1234
+    normalise_data(data = data, raw_data = raw_data, rep_samples = rep_samples, norm_clusters = cluster_list_rep_samples, k = k_value, num_clusters = clusters_nb, wd_data = wd_data, dir_norm_data = dir_name_norm_data)
+    print(getwd())
+
+    setwd("..")
+    unarcsinh_fcs_files(
+      "CytofRUV_output//CytofRUV_Norm_data_HC2_all_cl_20//",
+      "CytofRUV_output//CytofRUV_Norm_data_HC2_all_cl_20//"
+    )
+    path <- "CytofRUV_output//CytofRUV_Norm_data_HC2_all_cl_20//"
+
+    list_in_fcs_untransformed <- list.files(path, full.names = TRUE)
+    list2 <- list_in_fcs_untransformed[str_detect(list_in_fcs_untransformed, ".fcs")]
+    file.rename(list2, paste0(path, str_match(basename(list2), "(Batch_.+)")[, 1]))
+    list_in_fcs_untransformed <- list.files(path, full.names = TRUE)
+    list2 <- list_in_fcs_untransformed[str_detect(list_in_fcs_untransformed, ".fcs")]
+    file.copy(list2, copy_results_here, overwrite = TRUE)
+  })
 }
-2+2
-wd_data=file.path(getwd(),output_dir)
-print(wd_data)
-metadata_filename="Metadata.xlsx"
-panel_filename="Panel.xlsx"
-seed=1234
-clusters_nb=clusters_nb
-print("abc2")
-data <- load_data(wd_data,metadata_filename,panel_filename, cofactor = 5)
-print("abc1")
-print(data$daf)
-print(getwd())
-xx<- read_excel("Panel.xlsx")
-print(xx$antigen)
-#xx$fcs_colname
-print(data$lineage_markers)
-data$daf<- cluster_data(data$daf,seed,
-    markers_to_use=data$lineage_markers,
-    clusters_nb)
-
-daf<- data$daf
-md<- data$md
-seed=seed
-n_subset_marker_specific <- 1000
-print("abc")
-TSNE_subset <- 1000
-print("Running TSNE")
-daf <- CATALYST::runDR(daf, dr = "TSNE", cells = TSNE_subset)
-
-
-UMAP_subset <- 1000
-print("Running UMAP")
-daf <- runDR(daf, "UMAP", cells = UMAP_subset)
-
-n_subset <- 1000
-sub_daf <- daf[, sample(ncol(daf), n_subset)]
-
-panel<- data$panel    
 
 
 
-
-print("-1")
-dir_name_norm_data="CytofRUV_Norm_data_HC2_all_cl_20"
-print("0")
-print(data$daf)
-clusters = paste0("meta", as.character(clusters_nb))
-raw_data <- data.frame(sample = data$daf$sample_id, cluster=cluster_ids(data$daf,clusters), t(SummarizedExperiment::assay(data$daf,"exprs")))
-print("1")
-colnames(raw_data) <- gsub("^X", "",  colnames(raw_data))
-print("2")
-rep_samples=list(rep_samples)
-print("3")
-cluster_list_rep_samples <- list(seq(1,as.numeric(clusters_nb)))
-print(cluster_list_rep_samples)
-print("4")
-k_value <- 5
-seed=1234
-normalise_data(data=data,raw_data=raw_data,rep_samples=rep_samples, norm_clusters=cluster_list_rep_samples, k=k_value, num_clusters=clusters_nb,wd_data=wd_data,dir_norm_data=dir_name_norm_data)
-print(getwd())
-
-setwd("..")
-unarcsinh_fcs_files("CytofRUV_output//CytofRUV_Norm_data_HC2_all_cl_20//", 
-    "CytofRUV_output//CytofRUV_Norm_data_HC2_all_cl_20//")
-path = "CytofRUV_output//CytofRUV_Norm_data_HC2_all_cl_20//"
-
-list_in_fcs_untransformed <- list.files(path, full.names = TRUE)
-list2<- list_in_fcs_untransformed[str_detect(list_in_fcs_untransformed, ".fcs")]
-file.rename(list2, paste0(path, str_match(basename(list2), "(Batch_.+)")[ ,1]))
-list_in_fcs_untransformed <- list.files(path, full.names = TRUE)
-list2<- list_in_fcs_untransformed[str_detect(list_in_fcs_untransformed, ".fcs")]
-file.copy(list2, copy_results_here, overwrite = TRUE)
-})
-
-}
-
-
-
-#CytofRUV::launch_Shiny(daf)
+# CytofRUV::launch_Shiny(daf)
 
 
 
@@ -116,24 +118,25 @@ file.copy(list2, copy_results_here, overwrite = TRUE)
 # seed=1234
 
 # normalise_data(data=data,raw_data=raw_data,rep_samples=rep_samples, norm_clusters=cluster_list_rep_samples, k=k_value, num_clusters=clusters_nb,wd_data=wd_data,dir_norm_data=dir_name_norm_data)
-normalize_by_cytofruv<- function(rep_samples){
-try({
-print("-1")
-dir_name_norm_data="CytofRUV_Norm_data_HC2_all_cl_20"
-print("0")
-print(data)
-raw_data <- data.frame(sample = data$daf$sample_id, cluster=cluster_ids(data$daf,"meta20"), t(SummarizedExperiment::assay(data$daf,"exprs")))
-print("1")
-colnames(raw_data) <- gsub("^X", "",  colnames(raw_data))
-print("2")
-rep_samples=list(rep_samples)
-print("3")
-cluster_list_rep_samples <- list(seq(1,20))
-print("4")
-k_value <- 5
-seed=1234
-#normalise_data(data=data,raw_data=raw_data,rep_samples=rep_samples, norm_clusters=cluster_list_rep_samples, k=k_value, num_clusters=clusters_nb,wd_data=wd_data,dir_norm_data=dir_name_norm_data)
-print(getwd())})
+normalize_by_cytofruv <- function(rep_samples) {
+  try({
+    print("-1")
+    dir_name_norm_data <- "CytofRUV_Norm_data_HC2_all_cl_20"
+    print("0")
+    print(data)
+    raw_data <- data.frame(sample = data$daf$sample_id, cluster = cluster_ids(data$daf, "meta20"), t(SummarizedExperiment::assay(data$daf, "exprs")))
+    print("1")
+    colnames(raw_data) <- gsub("^X", "", colnames(raw_data))
+    print("2")
+    rep_samples <- list(rep_samples)
+    print("3")
+    cluster_list_rep_samples <- list(seq(1, 20))
+    print("4")
+    k_value <- 5
+    seed <- 1234
+    # normalise_data(data=data,raw_data=raw_data,rep_samples=rep_samples, norm_clusters=cluster_list_rep_samples, k=k_value, num_clusters=clusters_nb,wd_data=wd_data,dir_norm_data=dir_name_norm_data)
+    print(getwd())
+  })
 }
 
 
@@ -174,7 +177,7 @@ print(getwd())})
 # for (i in c(1:6)){
 #     anchor_vali<- read_excel("Metadata.xlsx")$sample_id[str_detect(read_excel("Metadata.xlsx")$sample_id, "anchor|vali")]
 #     results<- anchor_vali[str_detect(anchor_vali, as.character(i))]
-#     if (!identical(results, character(0)) ) { 
+#     if (!identical(results, character(0)) ) {
 #         #print(results)
 #         rep_samples<- append(rep_samples, results[1])
 #     }
@@ -219,7 +222,7 @@ print(getwd())})
 # daf <- runDR(daf, "UMAP", cells = UMAP_subset)
 # n_subset <- 1000
 # sub_daf <- daf[, sample(ncol(daf), n_subset)]
-# panel<- data$panel    
+# panel<- data$panel
 # dir_name_norm_data="CytofRUV_Norm_data_HC2_all_cl_20"
 # clusters = paste0("meta", as.character(clusters_nb))
 # raw_data <- data.frame(sample = data$daf$sample_id, cluster=cluster_ids(data$daf,clusters), t(SummarizedExperiment::assay(data$daf,"exprs")))
@@ -328,7 +331,7 @@ print(getwd())})
 
 # #rep_samples=list(c("HC2_B1","HC2_B2"))
 
-# output_dir="CytofRUV_output1"
+# output_dir="CytofRUV_output3"
 # if (!dir.exists(output_dir)){
 # dir.create(output_dir)
 # }
